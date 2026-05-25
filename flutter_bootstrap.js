d55f407003e941571d5acffdd7472597ff1303db
shy-r-jp <yuri.f.m.b.work@gmail.com>
2026-05-25 15:26:08 +0900

    バグ修正・UI統一・新背景追加・トランジション実装

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "494861306" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3162567247" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
