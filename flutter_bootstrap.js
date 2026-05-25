8fd62dc30996513eea15a15e7a2f78e2d8a2b54e
shy-r-jp <yuri.f.m.b.work@gmail.com>
2026-05-25 22:04:21 +0900

    縦横比固定表示の実装

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4185943018" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "742087435" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
